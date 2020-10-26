c56996e26efd904f6856f6fc93e4a486cecc5e98
efupiz <73142862+efupiz@users.noreply.github.com>
2020-10-26 13:35:26 +0200

    trs
    trs

--- sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', async event => {
-    console.log('Service worker has been installed');
-});
-
-
-self.addEventListener('fetch', event => {
-    console.log(`Trying to fetch ${event.request.url}`);
-    event.respondWith(checkCache(event.request));
-});self.addEventListener("install",async e=>{console.log("Service worker has been installed")}),self.addEventListener("fetch",e=>{console.log(`Trying to fetch ${e.request.url}`),e.respondWith(checkCache(e.request))});
